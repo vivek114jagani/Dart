718bf950d75032b07ddb4234522867d9bfb4c1c8
vivekjagani047@gmail.com <vivekjagani047@gmail.com>
2023-10-13 12:39:26 +0530

    DONE

--- Class and Object/password.dart
+++ Class and Object/password.dart
@@ -5,39 +5,7 @@ class PasswordGenerator {
     List<String> allalphabets =
         'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
     List<int> numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    List<String> specialCharacters = [
-      "!",
-      "@",
-      "#",
-      "₹",
-      "^",
-      "%",
-      "&",
-      "*",
-      "=",
-      "/",
-      "~",
-      "-",
-      "_",
-      "+",
-      ",",
-      ";",
-      ":",
-      "'",
-      ".",
-      "<",
-      ">",
-      "/",
-      "?",
-      "|",
-      "`",
-      "[",
-      "]",
-      "{",
-      "}",
-      "(",
-      ")"
-    ];
+    List<String> specialCharacters = ["!", "@", "#", "₹", "^", "%", "&", "*"];
     List<String> password = [];
     for (int i = 0; i < 5; i++) {
       password.add(allalphabets[Random().nextInt(allalphabets.length)]);
